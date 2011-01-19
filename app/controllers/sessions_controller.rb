5ec6cb227dc400ab4949a257af484d3d6f8eaafb
sebastian <syang@rocketfuelinc.com>
2011-01-18 17:53:40 -0800

    done with user edit/update

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,7 @@ class SessionsController < ApplicationController
       render 'new'
     else
       sign_in user
-      redirect_to user
+      redirect_back_or user
        
     end
     
@@ -22,6 +22,9 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    sign_out
+    redirect_to root_path
+    
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+  before_filter :authenticate, :only => [:edit, :update]
   def show
     @user = User.find(params[:id])
   end
@@ -7,6 +7,11 @@ class UsersController < ApplicationController
   def new
     @user = User.new
     @title = "Sign up"
+  end
+
+  def index
+    @title = "All users"
+    @users = User.all
   end
 
  def create
@@ -19,4 +24,46 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+ def edit
+   
+    @title = "Edit user"
+  end
+
+
+def update
+
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
+
+def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
+  end
+
+private
+
+    def authenticate
+      deny_access unless signed_in?
+    end
+
+     def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+
+def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
+
+
+
+     
 end
